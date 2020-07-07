1ed4913cdd77713ac57a348fa44c64074e0db36b
codeSourc3 <enzojmayo@gmail.com>
2020-07-06 23:30:31 -0500

    fixed naming and created stable build

---
+++ src/main/java/net/mcreator/darknether/item/DarkNetherEMSAItem.java
@@ -0,0 +1,39 @@
+
+package net.mcreator.darknether.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+
+import net.mcreator.darknether.world.dimension.DarkNetherEMSADimension;
+
+public class DarkNetherEMSAItem extends Item {
+	@ObjectHolder("dark_nether:dark_nether_emsa")
+	public static final Item block = null;
+	public DarkNetherEMSAItem() {
+		super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(64));
+	}
+
+	@Override
+	public ActionResultType onItemUse(ItemUseContext context) {
+		PlayerEntity entity = context.getPlayer();
+		BlockPos pos = context.getPos().offset(context.getFace());
+		ItemStack itemstack = context.getItem();
+		World world = context.getWorld();
+		if (!entity.canPlayerEdit(pos, context.getFace(), itemstack)) {
+			return ActionResultType.FAIL;
+		} else {
+			if (world.isAirBlock(pos))
+				DarkNetherEMSADimension.portal.portalSpawn(world, pos);
+			itemstack.damageItem(1, entity, c -> c.sendBreakAnimation(context.getHand()));
+			return ActionResultType.SUCCESS;
+		}
+	}
+}
